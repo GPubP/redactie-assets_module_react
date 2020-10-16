2f8202071cb147dfebadd502bc958e64431c488f
Glenn Verschooren <Glenn.Verschooren@digipolis.be>
2020-10-16 15:04:49 +0200

    feat: setup assets store

---
+++ public/lib/store/assets/assets.query.ts
@@ -0,0 +1,18 @@
+import { isNil } from '@datorama/akita';
+import { BaseEntityQuery } from '@redactie/utils';
+import { distinctUntilChanged, filter } from 'rxjs/operators';
+
+import { AssetsState } from './assets.model';
+import { assetsStore } from './assets.store';
+
+export class AssetsQuery extends BaseEntityQuery<AssetsState> {
+	public meta$ = this.select(state => state.meta).pipe(
+		filter(meta => !isNil(meta), distinctUntilChanged())
+	);
+	public assets$ = this.selectAll();
+	public asset$ = this.select(state => state.contentType).pipe(
+		filter(asset => !isNil(asset), distinctUntilChanged())
+	);
+}
+
+export const assetsQuery = new AssetsQuery(assetsStore);
---
+++ public/lib/store/assets/index.ts
@@ -0,0 +1,4 @@
+export * from './assets.model';
+export * from './assets.store';
+export * from './assets.query';
+export * from './assets.facade';
