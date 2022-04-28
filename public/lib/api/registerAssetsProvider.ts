e3f664090a56094ee57a798febc2a42e234f0f57
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2022-04-28 09:36:04 +0200

    feat: add hint

--- public/lib/api/registerAssetsProvider.ts
+++ public/lib/api/registerAssetsProvider.ts
@@ -1,4 +1,10 @@
 import { ExternalProviderOptions, externalProvidersFacade } from '../store/api/externalProviders';
 
+/**
+ * Register a external assets provider
+ * @param name A unique name to identity your external assets provider
+ * @param options options to setup your external assets provider
+ * @returns void
+ */
 export const registerAssetsProvider = (name: string, options: ExternalProviderOptions): void =>
 	externalProvidersFacade.registerProvider(name, options);
